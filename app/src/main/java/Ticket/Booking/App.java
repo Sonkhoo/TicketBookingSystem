1a6c40a3c501197773dbc3b835b1cf63aefb77b1
Sankhadeep Chowdhury <sankhadeepchowdhury5@gmail.com>
2025-05-22 16:07:47 +0530

    java baal

---
+++ app/src/main/java/Ticket/Booking/App.java
@@ -0,0 +1,10 @@
+package Ticket.Booking ;
+
+import java.util.*;
+public class App {
+
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
